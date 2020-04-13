9d022db297140a36c4751b69337c1637bd14641a
KimReady <kjb4380@gmail.com>
2020-04-14 00:03:16 +0900

    Add sample code for post/134

--- app/src/main/java/com/ready/blog/samples/MainActivity.kt
+++ app/src/main/java/com/ready/blog/samples/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.ready.blog.samples
 
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Slide
+import android.view.Gravity
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            with(window) {
+                requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+                // set an slide transition
+                enterTransition = Slide(Gravity.END)
+                exitTransition = Slide(Gravity.START)
+            }
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        first_activity_text.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
     }
 }
---
+++ app/src/main/java/com/ready/blog/samples/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.ready.blog.samples
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.Slide
+import android.view.Gravity
+import android.view.Window
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
