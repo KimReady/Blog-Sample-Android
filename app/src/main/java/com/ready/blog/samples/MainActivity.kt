73287ef8927cefbff490994093363bd33453beb4
KimReady <kjb4380@gmail.com>
2020-04-22 23:34:54 +0900

    Add basic WebView

--- app/src/main/java/com/ready/blog/samples/MainActivity.kt
+++ app/src/main/java/com/ready/blog/samples/MainActivity.kt
@@ -2,11 +2,16 @@ package com.ready.blog.samples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = web_view as WebView
+        webView.loadUrl("https://naver.com")
     }
 }
