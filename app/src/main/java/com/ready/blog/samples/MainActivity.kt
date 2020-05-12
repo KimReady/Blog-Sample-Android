120d750a2f60d90d6abd14e3695590eef725822d
KimReady <kjb4380@gmail.com>
2020-05-13 00:10:37 +0900

    add sample for backpress in fragment

--- app/src/main/java/com/ready/blog/samples/MainActivity.kt
+++ app/src/main/java/com/ready/blog/samples/MainActivity.kt
@@ -2,11 +2,15 @@ package com.ready.blog.samples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, SampleFragment()).commit()
     }
 }
--- app/src/main/java/com/ready/blog/samples/SampleFragment.kt
+++ app/src/main/java/com/ready/blog/samples/SampleFragment.kt
@@ -1,18 +1,30 @@
 package com.ready.blog.samples
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_sample.*
+
 
 class SampleFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sample, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                sample_text.text = "occur back pressed event!!"
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
 }
