7a9a3d020d51f713a8e9310aa4dd6cac9f4c90cc
KimReady <kjb4380@gmail.com>
2020-05-12 23:33:48 +0900

    add sample for backpress in fragment

---
+++ app/src/main/java/com/ready/blog/samples/SampleFragment.kt
@@ -0,0 +1,18 @@
+package com.ready.blog.samples
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SampleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sample, container, false)
+    }
+
+}
