3e3173c58f763e43b44921c74448382a187191c6
KimReady <kjb4380@gmail.com>
2021-03-17 02:14:56 +0900

    add sample for lifecycle of fragment

--- app/src/main/java/com/ready/blog/samples/MainActivity.kt
+++ app/src/main/java/com/ready/blog/samples/MainActivity.kt
@@ -2,9 +2,11 @@ package com.ready.blog.samples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+    val LOG_TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,10 +16,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun navigateTo(fragment: Fragment) {
+        Log.d(LOG_TAG, "add() - ${fragment.javaClass.simpleName}")
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container_view, fragment)           // 1. add
 //            .replace(R.id.fragment_container_view, fragment)     // 2. replace
-            .addToBackStack(null)                           // 3. addToBackStack
+//            .addToBackStack(null)                           // 3. addToBackStack
             .commit()
     }
 }
